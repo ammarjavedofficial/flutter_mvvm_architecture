1d8d453e560cc36851270871cf08e632db1d026e
AmmarJavedSwati <ammar.javed@swatitech.com>
2023-02-17 17:09:02 +0500

    flutter_mvvm_architecture

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'shared/app.dart';
 import 'shared/components/console_error_component.dart';
 
-
 final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
